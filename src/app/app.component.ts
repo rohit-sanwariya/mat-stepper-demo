346c682bb8351be81dea06f02ede7ab4363e70b2
Rohit Sanwariya <rohitsanwariya1995@gmail.com>
2024-01-31 13:50:01 +0530

    got to random step

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,23 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatButtonModule } from '@angular/material/button';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FormsModule, ReactiveFormsModule, MatInputModule, MatFormFieldModule, MatStepperModule, MatButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'mat-stepper-demo';
+
+  isLinear = false;
+  index = 0;
+
+ 
 }
